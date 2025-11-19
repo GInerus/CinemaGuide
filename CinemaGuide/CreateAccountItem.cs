befbec0b190434dc2bc0c210dcca23ce11112b1c
Герман Исаев <germanisaev422@gmail.com>
2025-11-19 17:43:46 +0300

    выделения

---
+++ CinemaGuide/CreateAccountItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace CinemaGuide
+{
+    public class CreateAccountItem
+    {
+        public Brush Color => Brushes.White;  // Стандартный серый цвет
+        public string Login => "Создать";
+        public string FirstCharacterOfLogin => "+";  // Плюсик вместо буквы
+        public Thickness TextMargin => new Thickness(0, 0, 0, 10); // Смещение для плюса
+    }
+}
