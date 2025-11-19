33c90462d30525ee689521b878234a3eb9dfc94f
Герман Исаев <germanisaev422@gmail.com>
2025-11-19 14:52:29 +0300

    Шаблоны для логинов. Коректно отображаются элипсы и подписи

---
+++ CinemaGuide/CircleItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace CinemaGuide
+{
+    public class CircleItem
+    {
+        public Brush Color { get; set; }
+        public String Login { get; set; }
+    }
+}
