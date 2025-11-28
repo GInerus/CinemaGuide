42cc7f63955bff0e797b5588e9f9d771f6be0baa
Герман Исаев <germanisaev422@gmail.com>
2025-11-28 19:38:59 +0300

    Обновлён AppDbContext для новых таблиц БД

--- CinemaGuide/Data/AppDbContext.cs
+++ CinemaGuide/Data/AppDbContext.cs
@@ -1,13 +1,14 @@
 ﻿using CinemaGuide.Models;
 using Microsoft.EntityFrameworkCore;
-using System.Windows.Input;
 
 namespace CinemaGuide.Data
 {
     public class AppDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
-
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<MovieGenre> MovieGenres { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
@@ -16,9 +17,25 @@ namespace CinemaGuide.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // Уникальный логин
             modelBuilder.Entity<User>()
                 .HasIndex(u => u.Username)
-                .IsUnique(); // Уникальный логин
+                .IsUnique();
+
+            // Составной ключ в MovieGenres
+            modelBuilder.Entity<MovieGenre>()
+                .HasKey(mg => new { mg.MovieId, mg.GenreId });
+
+            // Внешние ключи (SQLite сам создаст в таблице)
+            modelBuilder.Entity<MovieGenre>()
+                .HasOne<Movie>()
+                .WithMany()
+                .HasForeignKey(mg => mg.MovieId);
+
+            modelBuilder.Entity<MovieGenre>()
+                .HasOne<Genre>()
+                .WithMany()
+                .HasForeignKey(mg => mg.GenreId);
         }
     }
 }
