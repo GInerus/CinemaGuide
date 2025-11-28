d8815743286c55eb260ac5d16916b31804593273
Герман Исаев <germanisaev422@gmail.com>
2025-11-28 18:51:06 +0300

    Новые таблицы для бд: - Genres - Movies - MovieGenres (много ко многим)

---
+++ CinemaGuide/Models/Genre.cs
@@ -0,0 +1,8 @@
+﻿namespace CinemaGuide.Models
+{
+    public class Genre
+    {
+        public int GenreId { get; set; }
+        public required string Name { get; set; }
+    }
+}
---
+++ CinemaGuide/Models/MovieGenre.cs
@@ -0,0 +1,8 @@
+﻿namespace CinemaGuide.Models
+{
+    public class MovieGenre
+    {
+        public int MovieId { get; set; }
+        public int GenreId { get; set; }
+    }
+}
