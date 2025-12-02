15a7d8649d64d0b40ee1b8385a91a09779630169
Герман Исаев <germanisaev422@gmail.com>
2025-12-02 09:54:51 +0300

    Новая модель для таблицы Пользователь-Фильм

---
+++ CinemaGuide/Models/UserMovie.cs
@@ -0,0 +1,24 @@
+﻿namespace CinemaGuide.Models
+{
+    public class UserMovie
+    {
+        public int UserMovieId { get; set; }
+        public int UserId { get; set; }
+        public int MovieId { get; set; }
+        public UserMovieStatus Status { get; set; }
+        public int? Rating { get; set; }
+
+        public virtual User User { get; set; }
+        public virtual Movie Movie { get; set; }
+    }
+
+    public enum UserMovieStatus
+    {
+        None = 0,
+        Watching = 1,
+        Planned = 2,
+        Watched = 3,
+        Dropped = 4
+    }
+
+}
